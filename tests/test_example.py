dfeab2c6852ced4a86ffb453c759161547e2a681
Hein Bekker <hein@netbek.co.za>
2026-09-23 07:00:16 +0200

    Add query log test

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,9 +4,13 @@ from dw_lib.dbt import Dbt
 
 
 class TestExample(IntegrationTest):
-    def test_clickhouse_query(self, clickhouse_client: Client):
+    def test_clickhouse_version(self, clickhouse_client: Client):
         result = clickhouse_client.query("select version()")
         assert result.result_rows == [("26.3.33.24",)]
+
+    def test_clickhouse_query_log(self, clickhouse_client: Client):
+        result = clickhouse_client.query("select count() from system.query_log")
+        assert result.result_rows is not None
 
     def test_dbt_run(self, dbt: Dbt):
         runner_result = dbt.run(exclude="test_table")
