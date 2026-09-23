ce083471a901083f99023a08cbfdad1b0d571891
Hein Bekker <hein@netbek.co.za>
2026-09-23 06:43:10 +0200

    feat: Add ClickHouse test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,66 @@
+from clickhouse_connect.driver.client import Client
+from collections.abc import Generator
+from dw_lib.database import ClickHouseAdapter, ClickHouseSettings
 from dw_lib.dbt import Dbt
 from pathlib import Path
+from pydantic import BaseModel, Field
+from ruamel.yaml import YAML
+from typing import Any, Literal
 
 import pytest
 
 
+class DbtTargetSettings(BaseModel):
+    driver: Literal["http", "native"]
+    host: str
+    port: int
+    username: str = Field(alias="user")
+    password: str
+    database: str = Field(alias="schema")
+
+
+def to_clickhouse_settings(dbt_target_settings: DbtTargetSettings) -> ClickHouseSettings:
+    return ClickHouseSettings(
+        host=dbt_target_settings.host,
+        port=dbt_target_settings.port,
+        username=dbt_target_settings.username,
+        password=dbt_target_settings.password,
+        database=dbt_target_settings.database,
+    )
+
+
 class IntegrationTest:
-    @pytest.fixture
-    def profiles_dir(self) -> Path:
+    @pytest.fixture(scope="session")
+    def clickhouse_settings(self, dbt_profiles: dict) -> Generator[ClickHouseSettings, Any]:
+        dbt_target_settings = DbtTargetSettings(**dbt_profiles["example"]["outputs"]["dev"])
+        yield to_clickhouse_settings(dbt_target_settings)
+
+    @pytest.fixture(scope="session")
+    def clickhouse_adapter(
+        self, clickhouse_settings: ClickHouseSettings
+    ) -> Generator[ClickHouseAdapter, Any]:
+        yield ClickHouseAdapter(clickhouse_settings)
+
+    @pytest.fixture(scope="session")
+    def clickhouse_client(self, clickhouse_adapter: ClickHouseAdapter) -> Generator[Client, Any]:
+        with clickhouse_adapter.create_client() as client:
+            yield client
+
+    @pytest.fixture(scope="session")
+    def dbt_profiles_dir(self) -> Path:
         return Path(__file__).parent / "fixtures" / ".dbt"
 
-    @pytest.fixture
-    def project_dir(self) -> Path:
+    @pytest.fixture(scope="session")
+    def dbt_project_dir(self) -> Path:
         return Path(__file__).parent / "fixtures" / "dbt"
 
-    @pytest.fixture
-    def dbt(self, profiles_dir: Path, project_dir: Path) -> Dbt:
-        return Dbt(profiles_dir=profiles_dir, project_dir=project_dir)
+    @pytest.fixture(scope="session")
+    def dbt_profiles(self, dbt_profiles_dir: Path) -> dict:
+        yaml = YAML(typ="safe")
+        with open(dbt_profiles_dir / "profiles.yml") as fp:
+            data = yaml.load(fp)
+        return data
+
+    @pytest.fixture(scope="session")
+    def dbt(self, dbt_profiles_dir: Path, dbt_project_dir: Path) -> Generator[Dbt, Any]:
+        yield Dbt(profiles_dir=dbt_profiles_dir, project_dir=dbt_project_dir, target="dev")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,13 @@
 from .conftest import IntegrationTest
+from clickhouse_connect.driver.client import Client
 from dw_lib.dbt import Dbt
 
 
 class TestExample(IntegrationTest):
-    def test_run(self, dbt: Dbt):
+    def test_clickhouse_query(self, clickhouse_client: Client):
+        result = clickhouse_client.query("select count() from system.query_log")
+        print(result.result_rows)
+
+    def test_dbt_run(self, dbt: Dbt):
         runner_result = dbt.run(exclude="test_table")
         assert runner_result.success is True
